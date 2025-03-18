f12543f3024a201d49e40819bf9a2ff7cb30bf20
Elizebath <elizebaththomasv@gmail.com>
2025-03-18 09:50:37 +0530

    auth added

--- backend/middlewares/userAuthentication.js
+++ backend/middlewares/userAuthentication.js
@@ -4,7 +4,7 @@ require("dotenv").config()
 
 const userAuthentication=(req,res,next)=>{   
     
-    const {token} =req.cookies
+    const token = req.headers["authorization"].split(" ")[1];
     if(!token){
         throw new Error("User not authenticated")
     }
